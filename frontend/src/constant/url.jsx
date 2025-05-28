70a93a97fca2cd4b4cb55333bb19b886af77ed1d
Yohi-Stack <yohesmahendran@gmail.com>
2025-05-28 18:50:05 +0530

    second commit

--- frontend/src/constant/url.jsx
+++ frontend/src/constant/url.jsx
@@ -1,2 +1,2 @@
-export const baseUrl= "http://localhost:5000" ;
-// export const baseUrl = "https://xclone-backend.onrender.com";
+// export const baseUrl= "http://localhost:5000" ;
+export const baseUrl = "https://xclone-xlmr.onrender.com/login";
